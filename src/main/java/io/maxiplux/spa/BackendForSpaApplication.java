b3181a845f49d65425466bec7ab332491981d81c
Juan Francisco Mosquera <maxiplux@gmail.com>
2023-01-28 16:30:00 -0500

    Initial commit

---
+++ src/main/java/io/maxiplux/spa/BackendForSpaApplication.java
@@ -0,0 +1,13 @@
+package io.maxiplux.spa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendForSpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendForSpaApplication.class, args);
+    }
+
+}
