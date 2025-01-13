8094cd3eb17e8ba0127d8137fc52b9c1f164818e
Juan Francisco Mosquera <maxiplux@gmail.com>
2025-01-12 21:49:10 -0500

    openssl

--- src/main/java/io/maxiplux/spa/config/security/SecurityConfig.java
+++ src/main/java/io/maxiplux/spa/config/security/SecurityConfig.java
@@ -1,7 +1,6 @@
 package io.maxiplux.spa.config.security;
 
 import lombok.extern.slf4j.Slf4j;
-import org.opensaml.core.xml.schema.impl.XSStringImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,14 +9,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.AuthenticatedPrincipal;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.saml2.provider.service.authentication.OpenSaml4AuthenticationProvider;
-import org.springframework.security.saml2.provider.service.authentication.Saml2Authentication;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -33,43 +25,18 @@ public class SecurityConfig   {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        OpenSaml4AuthenticationProvider provider =
-                new OpenSaml4AuthenticationProvider();
 
-        provider.setResponseAuthenticationConverter(token -> {
-            var auth = OpenSaml4AuthenticationProvider
-                    .createDefaultResponseAuthenticationConverter()
-                    .convert(token);
-            log.info("AUTHORITIES: {}", auth.getAuthorities());
 
-            var attrValues = token.getResponse().getAssertions().stream()
-                    .flatMap(as -> as.getAttributeStatements().stream())
-                    .flatMap(attrs -> attrs.getAttributes().stream())
-                    .filter(attrs -> attrs.getName().equals("groups"))
-                    .findFirst().orElseThrow().getAttributeValues();
 
-            if (!attrValues.isEmpty()) {
 
-                var member = ((XSStringImpl) attrValues.get(0)).getValue();
-                log.info("MEMBER: {}", member);
-                List<GrantedAuthority> authoritiesList = List.of(
-                        new SimpleGrantedAuthority("ROLE_USER"),
-                        new SimpleGrantedAuthority("ROLE_" +
-                                member.toUpperCase().replaceFirst("/", ""))
-                );
-
-                log.info("NEW AUTHORITIES: {}", authoritiesList);
-                return new Saml2Authentication(
-                        (AuthenticatedPrincipal) auth.getPrincipal(),
-                        auth.getSaml2Response(),
-                        authoritiesList);
-            } else return auth;
-        });
 
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
-                        .antMatchers( "/css/**", "/js/**", "/images/**", "/public/**","/media/**").permitAll()
+                        .antMatchers( "/css/**", "/js/**", "/images/**", "/public/**","/media/**",
+                                "/webjars/**", "/favicon.ico", "/error", "/login", "/logout",
+                                "/saml2/**", "/saml2", "/saml2/metadata", "/saml2/login", "/saml2/logout"
+                                ).permitAll()
                         .anyRequest().authenticated()
                 )
                 .saml2Login(saml2 -> saml2
